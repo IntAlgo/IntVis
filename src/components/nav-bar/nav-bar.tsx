fc0703168bf5b3e96813ae5a505a73d7f87991db
Samip Shah <95608626+snarkyidiot@users.noreply.github.com>
2023-06-16 21:32:21 +0530

    changes

--- src/components/nav-bar/nav-bar.tsx
+++ src/components/nav-bar/nav-bar.tsx
@@ -18,7 +18,7 @@ export const NavBar = ({ className }: NavBarProps) => {
                 <button onClick={()=>{setMode("start")}} className={styles["navbar-item"]}>Start </button> | <button onClick={()=>{setMode("add")}} className={styles["navbar-item"]}>add</button> |{' '}
                 <button onClick={()=>setMode("edge")} className={styles["navbar-item"]} >edge//still in progress</button> | <button className={styles["navbar-item"]}>Contact Us</button>
             </nav>
-            <h3>Click to Add Node</h3>
+            {mode==="add"&&<h3>Click to Add Node</h3>}
             <h5>{mode}</h5>
         </div>
     );
